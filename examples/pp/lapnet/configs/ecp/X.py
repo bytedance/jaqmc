d44d66a5c190008ce35f27d62a6566f16014b5ea
revpba <mwkrev2501@icloud.com>
2026-03-16 14:14:51 +0900

    modify X.py
    adding charges and spin to be able to run ionization case

--- examples/pp/lapnet/configs/ecp/X.py
+++ examples/pp/lapnet/configs/ecp/X.py
@@ -5,18 +5,19 @@ from jaqmc.pp.pp_config import get_config as get_ecp_config
 from jaqmc.pp.ecp.data import load_ecp_variant as ecpvar
 
 def get_config(input_str):
-    symbol, spin, ecps = input_str.split(',')
-    spin = int(spin)
+    symbol, spin, charge, Xup, Xdn, ecps = input_str.split(',')
 
     cfg = base_config.default()
     cfg['ecp'] = get_ecp_config()
+
     mol = gto.Mole()
-    # Set up molecule
     mol.build(
         atom=f'{symbol} 0 0 0',
         basis={symbol: 'ccecpccpvdz'},
-        ecp={symbol: ecpvar(symbol,ecps)},
-        spin=spin)
+        ecp={symbol: ecpvar(symbol, ecps)},
+        spin=int(spin),
+        charge=int(charge))
 
     cfg.system.pyscf_mol = mol
+    cfg.system.atom_spin_configs = [(int(Xup), int(Xdn))]
     return cfg
