6b8e2ecd07b9b8d0c337f19db1b70524b54ea566
revpba <mwkrev2501@icloud.com>
2026-03-16 15:03:41 +0900

    fixing the electron count issue in XO.py
    to match the atom_spin_configs

--- examples/pp/lapnet/configs/ecp/XO.py
+++ examples/pp/lapnet/configs/ecp/XO.py
@@ -8,6 +8,11 @@ from jaqmc.pp.ecp.data import load_ecp_variant as ecpvar
 def get_config(input_str):
     symbol, dist, spin, charge, Xup, Xdn, Oup, Odn, ecps = input_str.split(',')
 
+    Xup = int(Xup)
+    Xdn = int(Xdn)
+    Oup = int(Oup)
+    Odn = int(Odn)
+
     cfg = base_config.default()
     cfg['ecp'] = get_ecp_config()
 
@@ -15,15 +20,18 @@ def get_config(input_str):
     mol.build(
         atom=f'{symbol} 0 0 0; O 0 0 {dist}',
         basis={symbol: 'ccecpccpvdz', 'O': 'ccpvdz'},
-        ecp={symbol: ecpvar(symbol, ecps), 'O': 'ccecp'},
+        ecp={symbol: ecpvar(symbol, ecps)},
         spin=int(spin),
         charge=int(charge),
     )
 
     cfg.system.pyscf_mol = mol
     cfg.system.atom_spin_configs = [
-        (int(Xup), int(Xdn)),
-        (int(Oup), int(Odn)),
+        (Xup, Xdn),
+        (Oup, Odn),
     ]
+
+    # keep JaQMC electron count consistent with atom_spin_configs
+    cfg.system.electrons = (Xup + Oup, Xdn + Odn)
 
     return cfg
