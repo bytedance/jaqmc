7da908b3e8a191d8d8a4608448dd336598b3e7f1
revpba <mwkrev2501@icloud.com>
2026-03-31 16:15:57 +0900

    add license header to special.py

--- jaqmc/pp/special.py
+++ jaqmc/pp/special.py
@@ -1,3 +1,9 @@
+# Copyright (c) ByteDance, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 import jax.numpy as jnp
 
 def legendre(x, l):
