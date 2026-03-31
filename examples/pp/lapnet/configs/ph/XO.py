52734102de75a812de45f48e0a7bcb703dc159f5
revpba <mwkrev2501@icloud.com>
2026-03-31 16:10:06 +0900

    adding license header/comments

--- examples/pp/lapnet/configs/ph/XO.py
+++ examples/pp/lapnet/configs/ph/XO.py
@@ -1,5 +1,8 @@
 # Copyright (c) ByteDance, Inc. and its affiliates.
 # All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
 
 from pyscf import gto
 
