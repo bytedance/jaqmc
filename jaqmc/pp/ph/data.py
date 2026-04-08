7769762882493bd889f32cd6d5ccb09b68b76aa7
revpba <mwkrev2501@icloud.com>
2026-04-08 21:25:09 +0900

    modify ph/data.py and complete makefile
    - add copyright notice to ecp/data.py and ph/data.py
    - add HPH_ELEMENTS for Sc
    - add new function gen_hph_data and gen_hph_info
    that are equal to gen_ph_data and gen_ph_info but
    now they are used for Sc with HPH implementation
    - edit gen_ph_data for Sc element only case,
    in the case of Sc HPH, ScO HPH, and ScS with Sc using HPH
    - fixing the wrapper function to get the ph_info
    and hph_info from config files
    - rearrange the function order in data.py for better readability
    - complete makefile for all testing cases

--- jaqmc/pp/ph/data.py
+++ jaqmc/pp/ph/data.py
@@ -1,7 +1,11 @@
+# Copyright (c) ByteDance, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 import numpy as np
 import pathlib
-
-# for reading L2 and semilocal data from XML file
 import re
 import xml.etree.ElementTree as ET
 
@@ -21,41 +25,40 @@ TM_ELEMENTS_INFO = {'Cr': (14.0, False),
                     'Ni': (18.0, True),
                     'Cu': (19.0, True),
                     'Zn': (20.0, False)}
-
 # value: spin
 PH_MG_ELEMENTS = {'S': 2}
 # value: (charge, use_hf)
 MG_ELEMENTS_INFO = {'S': (6.0, False)}
 
 PH_ELEMENTS = {**PH_TM_ELEMENTS, **PH_MG_ELEMENTS}
+HPH_ELEMENTS = {'Sc'}
 
 def PH_config(get_config,
               get_ecp_cfg_ref_from_cfg=lambda x: x.ecp,
               get_pyscf_mol_from_cfg=lambda x: x.system.pyscf_mol):
     def wrapper(*args):
         """
-        Wraps the user-defined config function to ensure PH (Pseudo-Hamiltonian)
-        information is properly initialized.
-    
-        Behavior:
-        - If `ph_info` is already explicitly defined in the config, the wrapper
-          preserves it and uses the provided `ph_info` and `ph_elements` as part
-          of the custom workflow.
-        - If `ph_info` is not defined, the wrapper falls back to the original
-          JaQMC workflow by automatically generating `ph_info` using
-          `gen_ph_info`, based on the atoms in the PySCF molecule and the
-          specified `ph_elements`.
-    
-        Returns:
-            cfg: Configuration object with initialized PH information.
+        Wrap a user config and automatically initialize PH/HPH information.
+
+        The user config is expected to declare only which atoms should use
+        pure PH (`ph_elements`) and/or hybrid PH (`hph_elements`).
+        The wrapper then generates `ph_info` and `hph_info` automatically
+        from the PySCF molecule.
         """
         cfg = get_config(*args)
         pyscf_mol = get_pyscf_mol_from_cfg(cfg)
         ecp_cfg_ref = get_ecp_cfg_ref_from_cfg(cfg)
-        if ecp_cfg_ref.ph_info is None:
+
+        if getattr(ecp_cfg_ref, 'ph_info', None) is None:
             ecp_cfg_ref.ph_info = gen_ph_info(
                 pyscf_mol._atom,
-                ph_elements=ecp_cfg_ref.ph_elements
+                ph_elements=getattr(ecp_cfg_ref, 'ph_elements', None),
+            )
+
+        if getattr(ecp_cfg_ref, 'hph_info', None) is None:
+            ecp_cfg_ref.hph_info = gen_hph_info(
+                pyscf_mol._atom,
+                hph_elements=getattr(ecp_cfg_ref, 'hph_elements', None),
             )
         return cfg
     return wrapper
@@ -102,60 +105,6 @@ def gen_ph_data_MG(element):
     # loc_data + charge = r * \tilde{v}_loc
     return loc_data + charge, l2_data
 
-def gen_ph_data(element):
-    if element in PH_TM_ELEMENTS:
-        return gen_ph_data_TM(element)
-    elif element in PH_MG_ELEMENTS:
-        return gen_ph_data_MG(element)
-    else:
-        raise NotImplementedError(f'The PH for {element} is not supported yet')
-
-def gen_ph_info(atoms, ph_elements=None):
-    def should_consider_ph(element):
-        # Try all available PH elements
-        if ph_elements is None:
-            return element in PH_ELEMENTS
-
-        return element in ph_elements
-
-    ph_atom_pos = []
-    ph_data = dict()
-    for symbol, pos in atoms:
-        if should_consider_ph(symbol):
-            ph_atom_pos.append((symbol, pos))
-            if symbol not in ph_data:
-                ph_data[symbol] = gen_ph_data(symbol)
-    return (ph_atom_pos, ph_data)
-
-def gen_ph_data_Co():
-    '''
-    This function generate Pseudo-Hamiltonian corresponding to
-    https://pubs.acs.org/doi/10.1021/acs.jctc.1c00992
-    , which only support element Co.
-
-    This PH is deprecated due to the presence of
-    https://pubs.aip.org/aip/jcp/article/159/16/164114/2918607/Locality-error-free-effective-core-potentials-for
-    , which support all the first row transition metals, including Co.
-    '''
-    def parse_xml(xml_file):
-        tree = ET.parse(xml_file)
-        root = tree.getroot()
-        loc_data = [float(y) for x in root[3][0][0][1].text.split('\n') for y in x.strip().split(' ') if y != '']
-        L2_data = [float(y) for x in root[2][0][1].text.split('\n') for y in x.strip().split(' ') if y != '']
-        return np.array(loc_data), np.array(L2_data)
-
-    xml_file = pathlib.Path(__file__).parent.resolve() / 'raw_data' / 'TM' / 'Co.pure.xml'
-    loc_data, l2_data = parse_xml(str(xml_file))
-
-    # The data in XML is in form:
-    #
-    # l2_data  = r * v_{L^2}
-    # loc_data + 17 = r * \tilde{v}_loc
-    #
-    # Here `l2_data` and `loc_data` are the data in "L2 section" and s-channel
-    # in "semilocal section" in the XML file respectively.
-    return loc_data + 17.0, l2_data
-
 def parse_compact_data(text):
     """
     Extract floating-point numbers from compact numeric text.
@@ -176,7 +125,6 @@ def extract_L2_data(xml_file):
         raise ValueError("L2/radfunc/data was not found in the XML file")
 
     text = tag.text
-
     tokens = re.findall(r"[+-]?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?", text)
 
     if len(tokens) == 0:
@@ -219,3 +167,89 @@ def load_sc_ph_data():
     l2_data = extract_L2_data(xml_file=str(xml_file))
 
     return dict(Sc=(loc_data + 11.0, l2_data))
+
+def gen_ph_data(element):
+    if element == 'Sc':
+        return load_sc_ph_data()['Sc']
+    elif element in PH_TM_ELEMENTS:
+        return gen_ph_data_TM(element)
+    elif element in PH_MG_ELEMENTS:
+        return gen_ph_data_MG(element)
+    else:
+        raise NotImplementedError(f'The PH for {element} is not supported yet')
+
+def gen_ph_info(atoms, ph_elements=None):
+    def should_consider_ph(element):
+        # Try all available PH elements
+        if ph_elements is None:
+            return element in PH_ELEMENTS
+        return element in ph_elements
+
+    ph_atom_pos = []
+    ph_data = dict()
+    for symbol, pos in atoms:
+        if should_consider_ph(symbol):
+            ph_atom_pos.append((symbol, pos))
+            if symbol not in ph_data:
+                ph_data[symbol] = gen_ph_data(symbol)
+    return (ph_atom_pos, ph_data)
+
+def gen_hph_data(element):
+    if element == 'Sc':
+        return load_sc_ph_data()['Sc']
+    else:
+        raise NotImplementedError(f'The HPH for {element} is not supported yet')
+
+def gen_hph_info(atoms, hph_elements=None):
+    def should_consider_hph(element):
+        if hph_elements is None:
+            return False
+        return element in hph_elements
+
+    hph_atom_pos = []
+    hph_data = dict()
+    for symbol, pos in atoms:
+        if should_consider_hph(symbol):
+            hph_atom_pos.append((symbol, pos))
+            if symbol not in hph_data:
+                hph_data[symbol] = gen_hph_data(symbol)
+    return (hph_atom_pos, hph_data)
+
+def gen_ph_data_Co():
+    '''
+    This function generate Pseudo-Hamiltonian corresponding to
+    https://pubs.acs.org/doi/10.1021/acs.jctc.1c00992
+    , which only support element Co.
+
+    This PH is deprecated due to the presence of
+    https://pubs.aip.org/aip/jcp/article/159/16/164114/2918607/Locality-error-free-effective-core-potentials-for
+    , which support all the first row transition metals, including Co.
+    '''
+    def parse_xml(xml_file):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        loc_data = [
+            float(y)
+            for x in root[3][0][0][1].text.split('\n')
+            for y in x.strip().split(' ')
+            if y != ''
+        ]
+        L2_data = [
+            float(y)
+            for x in root[2][0][1].text.split('\n')
+            for y in x.strip().split(' ')
+            if y != ''
+        ]
+        return np.array(loc_data), np.array(L2_data)
+
+    xml_file = pathlib.Path(__file__).parent.resolve() / 'raw_data' / 'TM' / 'Co.pure.xml'
+    loc_data, l2_data = parse_xml(str(xml_file))
+
+    # The data in XML is in form:
+    #
+    # l2_data  = r * v_{L^2}
+    # loc_data + 17 = r * \tilde{v}_loc
+    #
+    # Here `l2_data` and `loc_data` are the data in "L2 section" and s-channel
+    # in "semilocal section" in the XML file respectively.
+    return loc_data + 17.0, l2_data
